d25e412d29b0feb802285b42b6b3286e9bef9b0e
themoonr4 <themoonr4@gmail.com>
2026-03-22 13:35:04 +0530

    Create homepage for media startup

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Welcome to Our Media Startup</h1>
+            <p>Your go-to platform for the latest news and insights.</p>
+        </div>
+    );
+};
+
+export default HomePage;
